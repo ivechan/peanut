22c0dec88b47ab8503be34837274db31e793a149
Jing <lhchenjw@gmail.com>
2019-06-10 16:18:55 +0800

    add Parser.parse() function.

--- treesitter2/Parser.py
+++ treesitter2/Parser.py
@@ -1,13 +1,31 @@
-from _treesitter2.lib import *
+from _treesitter2.lib import (
+    ts_parser_new, ts_parser_delete, ts_parser_set_language,
+    ts_parser_parse, ts_parser_parse_string
+)
 from _treesitter2 import ffi
-from tree_sitter import Language
+from treesitter2.language import Language
+from treesitter2.tree import Tree
+
 
 class Parser(object):
     def __init__(self):
-        self.__parser = ts_parser_new()
+        self._cparser = ts_parser_new()
 
     def set_language(self, language: Language):
         return ts_parser_set_language(self.__parser, language.get_language_id())
+
+    def parse(self, string: bytes, old_tree: Tree = None):
+        """
+        string: bytes, must be utf-8 encoding.
+        old_tree: Tree
+        """
+        if Tree is not None:
+            ctree = ts_parser_parse_string(self._cparser, old_tree._ctree,
+                                           string, len(string))
+        else:
+            ctree = ts_parser_parse_string(self._cparser, ffi.NULL,
+                                           string, len(string))
+            return Tree(ctree)
 
     def __del__(self):
         ts_parser_delete(self.parser)
